14d407c869709bc234a7a541f079f0c688020bf2
anonboy096 <athicalhacker2005@gmail.com>
2025-01-18 20:50:40 +0530

    Create index.d.ts

---
+++ modules/index.d.ts
@@ -0,0 +1,4 @@
+export * from './albums/controllers/index.js';
+export * from './search/controllers/index.js';
+export * from './songs/controllers/index.js';
+export * from './artists/controllers/index.js';

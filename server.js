fe383a2457a29442c282aa5c497af184dbb63452
anonboy096 <athicalhacker2005@gmail.com>
2025-01-19 13:50:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 import { AlbumController, ArtistController, SearchController, SongController } from './modules/index.js';
 import { PlaylistController } from './modules/playlists/controllers/index.js';
 import { App } from './app.js';
-
-// Define the port number
-const PORT = process.env.PORT || 3000; // Use environment variable or default to 3000
-
-// Create an instance of the App with controllers
 const app = new App([
     new SearchController(),
     new SongController(),
@@ -13,10 +8,4 @@ const app = new App([
     new ArtistController(),
     new PlaylistController()
 ]).getApp();
-
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 export default app;
